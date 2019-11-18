cccaef159aa534a505448d8d7f9289ecfa7cc1dd
Yoti <realyoti@gmail.com>
2019-11-18 13:53:35 +0300

    compact and red

--- derinclocks.c
+++ derinclocks.c
@@ -14,21 +14,21 @@ int r1, r2;
 
 void clocks() {
 	kscePowerGetGpuEs4ClockFrequency(&r1, &r2);
-	blit_stringf(20, 50, "ARM: %d", kscePowerGetArmClockFrequency());
-	blit_stringf(20, 75, "BUS: %d", kscePowerGetBusClockFrequency());
-	blit_stringf(20, 100, "GPU: %d", kscePowerGetGpuClockFrequency());
-	blit_stringf(20, 125, "XBA: %d", kscePowerGetGpuXbarClockFrequency());
-	blit_stringf(20, 150, "ES4: %d", r1);
+	blit_stringf(4, 4, "ARM: %03d", kscePowerGetArmClockFrequency());
+	blit_stringf(4, 20, "BUS: %03d", kscePowerGetBusClockFrequency());
+	blit_stringf(4, 36, "GPU: %03d", kscePowerGetGpuClockFrequency());
+	blit_stringf(4, 52, "XBA: %03d", kscePowerGetGpuXbarClockFrequency());
+	blit_stringf(4, 68, "ES4: %03d", r1);
 }
 
 static tai_hook_ref_t ref_hook0;
-int _sceDisplaySetFrameBufInternalForDriver(int fb_id1, int fb_id2, const SceDisplayFrameBuf *pParam, int sync){
-	if(fb_id1 && pParam) {
+int _sceDisplaySetFrameBufInternalForDriver(int fb_id1, int fb_id2, const SceDisplayFrameBuf *pParam, int sync) {
+	if (fb_id1 && pParam) {
 		SceDisplayFrameBuf kfb;
-		memset(&kfb,0,sizeof(kfb));
+		memset(&kfb, 0, sizeof(kfb));
 		memcpy(&kfb, pParam, sizeof(SceDisplayFrameBuf));
 		blit_set_frame_buf(&kfb);
-		blit_set_color(0x0000FF00, 0xff000000);
+		blit_set_color(0x000000FF, 0xFF000000); // ABGR
 		clocks();
 	}
 		
@@ -40,7 +40,7 @@ int module_start(SceSize argc, const void *args) {
 	module_get_export_func(KERNEL_PID, "ScePower", 0x1590166F, 0x475BCC82, &_kscePowerGetGpuEs4ClockFrequency);
 	module_get_export_func(KERNEL_PID, "ScePower", 0x1590166F, 0x64641E6A, &_kscePowerGetGpuClockFrequency);
 
-	g_hooks[0] = taiHookFunctionExportForKernel(KERNEL_PID, &ref_hook0, "SceDisplay",0x9FED47AC,0x16466675, _sceDisplaySetFrameBufInternalForDriver); 
+	g_hooks[0] = taiHookFunctionExportForKernel(KERNEL_PID, &ref_hook0, "SceDisplay", 0x9FED47AC, 0x16466675, _sceDisplaySetFrameBufInternalForDriver); 
 
 	return SCE_KERNEL_START_SUCCESS;
 }
